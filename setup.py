12ec9d4fbf7181f6f1c432bacaf9e55897c627bc
Shivam Bansal <shivam5992@gmail.com>
2014-06-12 22:44:14 +0530

    url fixed

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name="pywordcloud",
-    version="0.1.1",
+    version="0.1",
     author="Shivam Bansal",
     author_email="shivam5992@gmail.com",
     packages=["pywordcloud"],
     include_package_data=True,
-    url="http://pypi.python.org/pypi/pywordcloud/",
+    url="https://github.com/shivam5992/pywordcloud",
     license='MIT',
     description="Generate Word Clouds in Python.",
     long_description=open("README.md").read(),
